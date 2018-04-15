45109963a38acc54a4e7bb4896357da6f536f74e
Vivek Singh <vivek.singh7891@outlook.com>
2018-04-16 00:30:42 +0200

    original commit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=testbot25041987-Web-Deploy.pubxml -p:Password=pLMNGAfEud66dlanyHa00GhAEflhNPssBrDfl8xiz7hgT9LlotCikr54FbiQ
+
